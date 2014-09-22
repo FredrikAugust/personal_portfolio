739824a8667a66e5091fd35babb78c48ab7f8236
Fredrik A. Madsen-Malmo <mail.fredrikaugust@gmail.com>
2014-09-22 20:58:27 +0200

    added html for front page

--- index.php
+++ index.php
@@ -5,5 +5,23 @@
 		<title>MrMadsenMalmo</title>
 	</head>
 	<body>
+    	<div class="site">
+        	<div class="front-page">
+            	<div class="content">
+                	<h1 class="name">Fredrik A. Madsen-Malmo</h1>
+                    <div class="short-border"></div>
+                    <h3>Web Designer &amp; Developer</h3>
+                    <div class="nav-button">
+                    	<span class="nav-button-text">Work</span>
+                    </div>
+                    <div class="nav-button">
+                    	<span class="nav-button-text">About</span>
+                    </div>
+                    <div class="nav-button">
+                    	<span class="nav-button-text">Contact</span>
+                    </div>
+                </div>
+            </div>
+		</div>
     </body>
 </html>
