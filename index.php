0325160ac9cec3ddcd8c53501535149f87595283
Fredrik A. Madsen-Malmo <mail.fredrikaugust@gmail.com>
2014-09-22 22:10:27 +0200

    added coloring and fonts

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <!doctype html>
 <html lang="en">
 	<head>
+    	<!-- Scripts for adobe typekit fonts -->
+    	<script src="//use.typekit.net/fgc6foa.js"></script>
+		<script>try{Typekit.load();}catch(e){}</script>
 		<meta charset="utf-8">
 		<title>MrMadsenMalmo</title>
         <link href="css/style.css" rel="stylesheet">
@@ -9,17 +12,17 @@
     	<div class="site">
         	<div class="front-page">
             	<div class="content">
-                	<h1 class="name raleway-bold">Fredrik A. Madsen-Malmo</h1>
+                	<h1 class="name">Fredrik A. Madsen-Malmo</h1>
                     <div class="short-border"></div>
-                    <h3 class="title raleway-light">Web Designer &amp; Developer</h3>
+                    <h3 class="title">Web Designer &amp; Developer</h3>
                     <div class="nav-button">
-                    	<span class="nav-button-text raleway-light">Work</span>
+                    	<span class="nav-button-text">Work</span>
                     </div>
                     <div class="nav-button">
-                    	<span class="nav-button-text raleway-light">About</span>
+                    	<span class="nav-button-text">About</span>
                     </div>
                     <div class="nav-button">
-                    	<span class="nav-button-text raleway-light">Contact</span>
+                    	<span class="nav-button-text">Contact</span>
                     </div>
                 </div>
             </div>
