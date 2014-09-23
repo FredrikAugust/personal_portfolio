6e8d12f61254c607c6f65f915c9c9f007c3177db
Fredrik A. Madsen-Malmo <mail.fredrikaugust@gmail.com>
2014-09-23 19:30:57 +0200

    reduced amount of code

--- index.php
+++ index.php
@@ -9,23 +9,21 @@
         <link href="css/style.css" rel="stylesheet">
 	</head>
 	<body>
-    	<div class="site">
-        	<div class="front-page">
-            	<div class="content">
-                	<h1 class="name">Fredrik A. Madsen-Malmo</h1>
-                    <div class="short-border"></div>
-                    <h3 class="title">Web Designer &amp; Developer</h3>
-                    <div class="nav-button">
-                    	<span class="nav-button-text">Work</span>
-                    </div>
-                    <div class="nav-button">
-                    	<span class="nav-button-text">About</span>
-                    </div>
-                    <div class="nav-button">
-                    	<span class="nav-button-text">Contact</span>
-                    </div>
+        <div class="front-page site">
+            <div class="content">
+                <h1 class="name">Fredrik A. Madsen-Malmo</h1>
+                <div class="short-border"></div>
+                <h3 class="title">Web Designer &amp; Developer</h3>
+                <div class="nav-button">
+                    <span class="nav-button-text">Work</span>
+                </div>
+                <div class="nav-button">
+                    <span class="nav-button-text">About</span>
+                </div>
+                <div class="nav-button">
+                    <span class="nav-button-text">Contact</span>
                 </div>
             </div>
-		</div>
+        </div>
     </body>
 </html>
