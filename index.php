5c4f247f83646ec86e2931f5c20752a163fbc7b9
Fredrik A. Madsen-Malmo <mail.fredrikaugust@gmail.com>
2014-09-23 20:59:06 +0200

    added basic about page

--- index.php
+++ index.php
@@ -25,5 +25,17 @@
                 </div>
             </div>
         </div>
+        <div class="center site">
+            <div class="content2">
+                <h1 class="about">About Me</h1>
+                <div class="short-border"></div>
+                <div class="about-info clearfix">
+                	<img src="http://placehold.it/1000x500" alt="me-img">
+                    <div class="about-text">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
