ff886f56e4e5e6ebbb4d372f50698f9ffe8a0043
Fredrik A. Madsen-Malmo <mail.fredrikaugust@gmail.com>
2014-09-23 21:05:29 +0200

    positioned the about page

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
                 <h1 class="about">About Me</h1>
                 <div class="short-border"></div>
                 <div class="about-info clearfix">
-                	<img src="http://placehold.it/1000x500" alt="me-img">
+                	<img src="http://placehold.it/420x258" alt="me-img">
                     <div class="about-text">
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                     </div>
