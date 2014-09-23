74bdfabce6cfef173dd5502b116227408d332351
Fredrik A. Madsen-Malmo <mail.fredrikaugust@gmail.com>
2014-09-23 21:16:50 +0200

    added social icon placeholders

--- index.php
+++ index.php
@@ -35,6 +35,12 @@
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                     </div>
                 </div>
+                <div class="short-border"></div>
+                <div class="contact-icons">
+                	<img src="http://placehold.it/40x40"><!-- Twitter -->
+                    <img src="http://placehold.it/40x40"><!-- GitHub -->
+                    <img src="http://placehold.it/40x40"><!-- Mail -->
+                </div>
             </div>
         </div>
     </body>
