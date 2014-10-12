5d35c71992c473d4f99433a12353aa5b723b3a3b
Fredrik A. Madsen-Malmo <mail.fredrikaugust@gmail.com>
2014-10-12 22:12:43 +0200

    added comments

--- script.js
+++ script.js
@@ -1,27 +1,31 @@
 // JavaScript Document
 
+// Hide the arrow that takes you to the top of the page
 $("#arrow").css("visibility", "hidden");
 
+// When you click one of the navigation buttons
 $(".nav-button").click(function() {
-	var item1 = $(this).data("ref");
-    $('html, body').animate({
-        scrollTop: $(item1).offset().top
-    }, 1000);
+	var item1 = $(this).data("ref"); // Set the var item1 to the data-ref value of the clicked item
+    $('html, body').animate({ // Animate html and body
+        scrollTop: $(item1).offset().top // Scroll the length from item1 to the top
+    }, 1000); // Animation duration is one second
 });
 
+// When you scroll the window do the following
 $(window).scroll(function(e) {
-	var scrollpos = $(window).scrollTop();
-	var about_scroll = $("#about").offset().top;
-	
-	if (scrollpos >= about_scroll) {
-		$("#arrow").fadeIn(300).css("visibility", "visible");
+	var scrollpos = $(window).scrollTop(); // The var scrollpos is the distance from window to the top
+	var about_scroll = $("#about").offset().top; // The length from the about page to the top of the page
+
+	if (scrollpos >= about_scroll) { // If the scrollpos is more or equal to about_scroll
+		$("#arrow").fadeIn(300).css("visibility", "visible"); // Fade in the arrow
 	} else {
-		$("#arrow").fadeOut(300);
+		$("#arrow").fadeOut(300); // If not, fade it out
 	}
 });
 
+// When you click the arrow
 $("#arrow").on("click", function() {
-    $('html, body').animate({
-        scrollTop: -($("#arrow").offset().top)
-    }, 2000, "swing");
+    $('html, body').animate({ // Animate html and body
+        scrollTop: -($("#arrow").offset().top) // Scroll the remainder to the top upwards
+    }, 2000, "swing"); // Animation duration is two seconds with the animation "version" swing
 });
